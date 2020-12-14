5213ac81f8d64230fc26b29152c7f3995f6d8645
speeds0n <edson.macaiba@hotmail.com>
2020-12-14 07:41:10 -0300

    nextpoint ok

--- lib/snake.h
+++ lib/snake.h
@@ -4,6 +4,12 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+//#include <vector>
+#include <deque>
+#include <thread>
+#include <chrono>
+#include <ctime>
+#include <cstdlib>
 
 class Snake{
 
@@ -21,12 +27,16 @@ class Snake{
 		void loss_life();
 };
 
+
 class Level{
 
 	private:
 		int m_line;
 		int m_colmn;
-		std::string m_maze[3] = {"#", ".", "*"};
+		int m_spawn_x;
+		int m_spawn_y;
+//		Player m_player;
+		std::deque<std::string> m_maze;
 
 	public:
 		Level()
@@ -35,14 +45,62 @@ class Level{
 
 		int get_line(){ return m_line; }
 		int get_colmn(){ return m_colmn; }
+		int get_spawn_x(){ return m_spawn_x; }
+		int get_spawn_y(){ return m_spawn_y; }
 		void set_line(int value);
-		void set_column(int value);
+		void set_colmn(int value);
+		void set_spawn_x(int value);
+		void set_spawn_y(int value);
+		void add_line(std::string value);
+		void delete_maze();
+		void print_maze();
+		bool check_spawn();
 };
 
 class Player{
 
-	bool find_solution();
-	//Direction
+	private:
+		int m_position_x;
+		int m_position_y;
+		int m_line;
+		int m_colmn;
+		int m_point_x;
+		int m_point_y;
+
+		std::deque<std::string> m_maze;
+
+		struct Direction{
+			int x;
+			int y;
+
+		};
+		std::deque<Direction> m_solution;
+
+	public:
+
+		int get_position_x(){ return m_position_x; }
+		int get_position_y(){ return m_position_y; }
+		int get_line(){ return m_line; }
+		int get_colmn(){ return m_colmn; }
+		int get_point_x(){ return m_point_x; }
+		int get_point_y(){ return m_point_y; }
+		int solution_size(){ return m_solution.size(); }
+
+		void set_position_x(int value);
+		void set_position_y(int value);
+		void set_line(int value);
+		void set_colmn(int value);
+		void set_point_x(int value);
+		void set_point_y(int value);
+
+		void add_line(std::string value);
+		Direction next_move();
+		bool find_solution(std::deque<std::string> maze, int x, int y);
+		void print_maze();
+		void delete_maze();
+		void generate_food();
+		void delete_food(int x, int y);
+		void solve(int x, int y);
 };
 
 class SnakeGame{
@@ -51,14 +109,23 @@ class SnakeGame{
 		int m_process;
 		int m_status;
 		Level m_level;
+		Player m_player;
 		std::string m_file_name;
+		std::ifstream m_file;
+		std::string m_line;
+		std::string m_temp;
 
 	public:
 
 		enum GameProcess{
-			UNINFORMED_FILE = 1,
-			REPORTED_FILE,
-			FILE_DIDNT_OPEN
+			UNINFORMED_FILE = 1,	// 1
+			REPORTED_FILE,			// 2
+			FILE_DIDNT_OPEN,		// 3
+			FILE_OPEN,				// 4
+			END_OF_FILE,			// 5
+			CURRENT_LEVEL,			// 6
+			NEXT_LEVEL,				// 7
+			NO_FOUND_SPAWN			// 8
 		};
 
 		enum GameStatus{
@@ -66,7 +133,7 @@ class SnakeGame{
 			CONTINUE_GAME
 		};
 
-		int get_process(){ return m_error; }
+		int get_process(){ return m_process; }
 		int get_status(){ return m_status; }
 		std::string get_file_name(){ return m_file_name; }
 		void set_process(int value);
@@ -74,6 +141,8 @@ class SnakeGame{
 		void set_file_name(std::string value);
 
 		void initialize_game(int argc, char *argv[]);
+		void open_file(std::ifstream &file);
+		void read_file(std::ifstream &file);
 		void process();
 		void update();
 		void render();
--- src/snake.cpp
+++ src/snake.cpp
@@ -10,14 +10,204 @@ void Snake::loss_life(){
 	m_life--;
 }
 
+/// Class Player ///
+
+void Player::set_position_x(int value){
+	m_position_x = value;
+}
+void Player::set_position_y(int value){
+	m_position_y = value;
+}
+
+void Player::set_line(int value){
+	m_line = value;
+}
+
+void Player::set_colmn(int value){
+	m_colmn = value;
+}
+
+void Player::set_point_x(int value){
+	m_point_x = value;
+}
+
+void Player::set_point_y(int value){
+	m_point_y = value;
+}
+Player::Direction Player::next_move(){
+	Direction aux;
+	aux.x = m_solution.front().x;
+	aux.y = m_solution.front().y;
+	m_solution.pop_front();
+	return aux;
+}
+// solve deve rebecer a do spawn
+void Player::solve(int x, int y){
+	if(find_solution(m_maze, x, y)){
+		//GERAR UMA NOVA COMIDA
+		std::cout << "GERAR UMA NOVA COMIDA" << std::endl;
+		std::cout << "GERAR UMA NOVA COMIDA" << std::endl;
+		std::cout << "GERAR UMA NOVA COMIDA" << std::endl;
+		std::cout << "GERAR UMA NOVA COMIDA" << std::endl;
+	}
+}
+
+bool Player::find_solution(std::deque<std::string> maze, int x, int y){
+
+	if(maze[x][y] == 'x'){
+		//print_solution();
+		return true;
+	}
+	if(maze[x][y] == '#' || maze[x][y] == '$'){
+		return false;
+	}
+	maze[x][y] = '$';
+	Direction dir = {x, y};
+	m_solution.push_back(dir);
+
+	bool result;
+
+	result = find_solution(maze, x, y+1);
+	if(result){	
+
+		return true;
+	}
+	result = find_solution(maze, x-1, y);
+	if(result){
+
+		return true;
+	}
+	result = find_solution(maze, x, y-1);
+	if(result){
+
+		return true;
+	}
+	result = find_solution(maze, x+1, y);
+	if(result){
+
+		return true;
+	}
+
+	maze[x][y] = ' ';
+	return false;
+}
+
+void Player::add_line(std::string value){
+	m_maze.push_back(value);
+}
+
+void Player::print_maze(){
+
+	int i{0}, j{0};
+
+	while(i < get_line()){
+		j = 0;
+		while(j < get_colmn()){
+			if(i == get_position_x() && j == get_position_y()){
+				std::cout << ">";
+			}else{
+				std::cout << m_maze[i][j];
+			}
+			j++;
+		}
+		std::cout << std::endl;
+		i++;
+	}
+}
+
+void Player::delete_maze(){
+	m_maze.clear();
+	set_line(0);
+	set_colmn(0);
+}
+
+void Player::generate_food(){
+
+	int point_x, point_y;
+	bool aux = false;
+
+	srand(time(NULL));
+	while(!aux){
+		point_x = rand() % get_line();
+		point_y = rand() % get_colmn();
+			if(m_maze[point_x][point_y] == ' '){
+			set_point_x(point_x);
+			set_point_y(point_y);
+			aux = true;
+		}
+	}
+	m_maze[point_x][point_y] = 'x';
+}
+
+void Player::delete_food(int x, int y){
+	m_maze[x][y] = ' ';
+}
 /// Class Level ///
 void Level::set_line(int value){
 	m_line = value;
 }
 
-void Level::set_column(int value){
+void Level::set_colmn(int value){
 	m_colmn = value;
 }
+
+void Level::set_spawn_x(int value){
+	m_spawn_x = value;
+}
+
+void Level::set_spawn_y(int value){
+	m_spawn_y = value;
+}
+
+void Level::add_line(std::string value){
+	m_maze.push_back(value);
+}
+
+void Level::delete_maze(){
+	m_maze.clear();
+	set_line(0);
+	set_colmn(0);
+}
+
+void Level::print_maze(){
+
+	int i{0}, j{0};
+
+	while(i < get_line()){
+		j = 0;
+		while(j < get_colmn()){
+
+			if(m_maze[i][j] == '$'){
+				std::cout << " ";
+			}else{
+				std::cout << m_maze[i][j];
+			}
+			j++;
+		}
+		std::cout << std::endl;
+		i++;
+	}
+}
+
+bool Level::check_spawn(){
+
+	int i{0}, j{0};
+	
+	while(i < get_line()){
+		j = 0;
+		while(j < get_colmn()){
+			if(m_maze[i][j] == '*'){
+				set_spawn_x(i);
+				set_spawn_y(j);
+				return true;
+			}
+			j++;
+		}
+		i++;
+	}
+	return false;
+}
+
 /// Class SnakeGame ///
 
 void SnakeGame::set_process(int value){
@@ -42,20 +232,69 @@ void SnakeGame::initialize_game(int argc, char *argv[]){
 	}
 }
 
+void SnakeGame::open_file(std::ifstream &file){
+
+	file.open(get_file_name());
+}
+
+void SnakeGame::read_file(std::ifstream &file){
+	
+	int i{0};
+
+	if(file.eof()){
+		set_process(5);
+	}else{
+		getline(file, m_line);
+		std::cout << m_line << std::endl;
+		while(i < m_line.size()){
+			if(m_line.compare(i, 1, " ") == 0){
+				
+				m_temp = m_line.substr(0, i);
+				m_level.set_line(std::stoi(m_temp));
+				m_temp = m_line.substr(i+1, m_line.size());
+				m_level.set_colmn(std::stoi(m_temp));
+			}
+			i++;
+		}
+
+		i = 0;
+		while(i < m_level.get_line()){
+			getline(file, m_line);
+			m_level.add_line(m_line);
+			m_player.add_line(m_line);
+			i++;
+		}
+	}
+}
+
 void SnakeGame::process(){
 
 	if(get_process() == GameProcess::REPORTED_FILE){
 
-		file.open(get_file_name());
+		open_file(m_file);
 
-		if(file.is_open()){
-
-			set_status(2);
-			getline(file, temp);
+		if(m_file.is_open()){
+			// FILE OPEN
+			set_process(4);
 
 		}else{
-
-			set_status(1);
+			// FILE DID NOT OPEN
+			set_process(3);
+		}
+	}else if(get_process() == GameProcess::CURRENT_LEVEL){
+		if(m_player.solution_size() > 0){
+			auto dir = m_player.next_move();
+			m_player.set_position_x(dir.x);
+			m_player.set_position_y(dir.y);
+		//	std::cout << m_player.solution_size() << std::endl;
+		//	if(m_player.get_position_x() == m_player.get_point_x() &&
+		//	m_player.get_position_y() == m_player.get_point_y()){
+		//		std::cout << "TESTE" << std::endl;
+		//	}
+		}else{
+			m_player.delete_food(m_player.get_point_x(), m_player.get_point_y());
+			m_player.generate_food();
+			m_player.solve(m_player.get_position_x(), m_player.get_position_y());
 		}
 	}
 }
@@ -63,9 +302,53 @@ void SnakeGame::process(){
 void SnakeGame::update(){
 	
 	if(get_process() == GameProcess::UNINFORMED_FILE){
+
 		set_status(1);
+
 	}else if(get_process() == GameProcess::FILE_DIDNT_OPEN){
+
 		set_status(1);
+
+	}else if(get_process() == GameProcess::FILE_OPEN){
+
+		read_file(m_file);
+		set_status(2);
+		if(m_level.check_spawn()){
+			m_player.set_position_x(m_level.get_spawn_x());
+			m_player.set_position_y(m_level.get_spawn_y());
+			m_player.set_line(m_level.get_line());
+			m_player.set_colmn(m_level.get_colmn());
+			m_player.generate_food();
+			m_player.solve(m_level.get_spawn_x(), m_level.get_spawn_y());
+			set_process(6);
+		}else{
+			set_process(8);
+		}
+
+	}else if(get_process() == GameProcess::END_OF_FILE){
+
+		m_file.close();
+		set_status(1);
+
+	}else if(get_process() == GameProcess::CURRENT_LEVEL){
+
+		set_status(2);
+		std::chrono::milliseconds dura(1000/15);
+		std::this_thread::sleep_for(dura);
+
+	}else if(get_process() == GameProcess::NEXT_LEVEL){
+
+		m_level.delete_maze();
+		m_player.delete_maze();
+
+		set_process(4);
+		set_status(2);
+
+		if(m_file.eof()){
+			set_process(5);
+		}
+	}else if(get_process() == GameProcess::NO_FOUND_SPAWN){
+		set_process(7);
 	}
 }
 
@@ -75,11 +358,20 @@ void SnakeGame::render(){
 		std::cout << "Name file uniformed" << std::endl;
 	}else if(get_process() == GameProcess::FILE_DIDNT_OPEN){
 		std::cout << "File did not open" << std::endl;
+	}else if(get_process() == GameProcess::FILE_OPEN){
+//		m_level.print_maze();
+//		m_level.delete_maze();
+	}else if(get_process() == GameProcess::CURRENT_LEVEL){
+		m_player.print_maze();
+	}else if(get_process() == GameProcess::NEXT_LEVEL){
+		std::cout << "Next Level" << std::endl;
+//		m_level.delete_maze();
+	}else if(get_process() == GameProcess::NO_FOUND_SPAWN){
+		std::cout << "Spawn not found, going to next level." << std::endl;
 	}
 }
 
 bool SnakeGame::game_over(){
 
 	return get_status() == GameStatus::EXIT_GAME;
-
 }
